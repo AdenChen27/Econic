a2e004217ecbe9226e2f28c28d6423d1181e7bbc
AdenChen27 <AdenChen2005@gmail.com>
2023-05-23 15:54:21 +0800

    DS_curve: Supply Curve Intro animation #1,2

--- SD_curves.py
+++ SD_curves.py
@@ -102,7 +102,7 @@ class DemandCurveIntro(Scene):
     # Animation #2: demand curve shifting
     """
     def construct(self):
-        self.init() # +{dc_graph, sc_graph} -> {u_graph}
+        self.init() # +{dc, dc_graph, dc_label} -> {dc, dc_graph, dc_label}
         # self.animation_1() # no change
         self.animation_2() # no change
 
@@ -176,7 +176,85 @@ class DemandCurveIntro(Scene):
         self.play(dc_graph.animate.shift(RIGHT), dc_label.animate.shift(RIGHT))
 
         
+class SupplyCurveIntro(Scene):
+    """
+    Demand Curve Introduction
 
+
+    # Animation #1: moving along the supply curve
+    # Animation #2: supply curve shifting
+    """
+    def construct(self):
+        self.init() # +{sc, sc_graph, sc_label} -> {sc, sc_graph, sc_label}
+        # self.animation_1() # no change
+        self.animation_2() # no change
+
+    def clean(self):
+        for m in self.mobjects:
+            if m not in self.protected_mobjects:
+                self.remove(m)
+
+    def add_mobjects(self, *args, animation=FadeIn, **kwargs):
+        if self.FADE_ANIMATION_OFF and animation == FadeIn:
+            animation = None
+        if animation is None:
+            self.add(*args)
+        else:
+            self.play(animation(Group(*args)), **kwargs)
+
+    def remove_mobjects(self, *args, animation=FadeOut, **kwargs):
+        if self.FADE_ANIMATION_OFF and animation == FadeOut:
+            animation = None
+        if animation is not None:
+            self.play(animation(Group(*args)), **kwargs)
+        self.remove(*args)
+
+    def init(self):
+        self.FADE_ANIMATION_OFF = False
+
+        plane = Axes(**AX_CONFIG).shift(LEFT*2)
+        plane_labels = plane.get_axis_labels(x_label="Q", y_label="P")
+        self.add(plane, plane_labels)
+
+        sc = LinearFunction(plane, 1, 0, [3, 7])
+
+        sc_graph = sc.get_graph()
+        sc_label = Text("S").scale(.5).next_to(plane.c2p(7, 7))
+
+        self.add(sc_graph, sc_label)
+
+        self.plane, self.sc, self.sc_graph, self.sc_label = plane, sc, sc_graph, sc_label
+        # self.sc, self.sc_graph = sc, sc_graph
+        self.protected_mobjects = [self.plane, self.sc_graph, plane_labels, sc_label]
+
+    def animation_1(self):
+        # Animation #1: moving along the supply curve
+        plane, sc, sc_graph, sc_label = self.plane, self.sc, self.sc_graph, self.sc_label
+        x_tracker = ValueTracker(3.5)
+        
+        line_to_x_axis = DashedLine().add_updater(lambda l: l.put_start_and_end_on(
+            sc.get_pos(x=x_tracker.get_value()), 
+            plane.c2p(x_tracker.get_value(), 0)
+        ))
+        dot = Dot().add_updater(
+            lambda d: d.move_to(sc.get_pos(x=x_tracker.get_value()))
+        )
+        q_label = Text("Q").scale(.5).add_updater(
+            lambda t: t.next_to(plane.c2p(x_tracker.get_value(), 0), DOWN)
+        )
+        self.add(line_to_x_axis, q_label, dot)
+
+        for x in [6.5, 3.5]:
+            self.play(x_tracker.animate.set_value(x), run_time=2)
+
+        self.clean()
+
+    def animation_2(self):
+        # Animation #2: supply curve shifting
+        plane, sc, sc_graph, sc_label = self.plane, self.sc, self.sc_graph, self.sc_label
+        self.play(sc_graph.animate.shift(RIGHT), sc_label.animate.shift(RIGHT))
+        self.play(sc_graph.animate.shift(LEFT*2), sc_label.animate.shift(LEFT*2))
+        self.play(sc_graph.animate.shift(RIGHT), sc_label.animate.shift(RIGHT))
 
 
 
--- out/sd_curves.md
+++ out/sd_curves.md
@@ -15,3 +15,17 @@ Animation \#1: moving along the demand curve
 Animation \#2: demand curve shifting
 
 ![DemandCurveIntro-2](DemandCurveIntro-2.gif)
+
+
+## Supply Curve Introduction
+
+Animation \#1: moving along the supply curve
+
+![SupplyCurveIntro-1](SupplyCurveIntro-1.gif)
+
+Animation \#2: sypply curve shifting
+
+![SupplyCurveIntro-2](SupplyCurveIntro-2.gif)
+
+
+
